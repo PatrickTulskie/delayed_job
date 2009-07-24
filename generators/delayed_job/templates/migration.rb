05bf0cd7a61c7c92f0651ce173501ed6ae8e8c85
patricktulskiem <patricktulskie@gmail.com>
2009-07-24 12:20:28 -0400

    Bumped version to 1.8.1, restructured the initial migration template and added fields for helder's code.  Defaulted queue over to not deleting old jobs in the initializer.  README has been updated to reflect changes.

--- generators/delayed_job/templates/migration.rb
+++ generators/delayed_job/templates/migration.rb
@@ -1,15 +1,18 @@
 class CreateDelayedJobs < ActiveRecord::Migration
   def self.up
-    create_table :delayed_jobs, :force => true do |table|
-      table.integer  :priority, :default => 0      # Allows some jobs to jump to the front of the queue
-      table.integer  :attempts, :default => 0      # Provides for retries, but still fail eventually.
-      table.text     :handler                      # YAML-encoded string of the object that will do work
-      table.text     :last_error                   # reason for last failure (See Note below)
-      table.datetime :run_at                       # When to run. Could be Time.now for immediately, or sometime in the future.
-      table.datetime :locked_at                    # Set when a client is working on this object
-      table.datetime :failed_at                    # Set when all retries have failed (actually, by default, the record is deleted instead)
-      table.string   :locked_by                    # Who is working on this object (if locked)
-      table.timestamps
+    create_table :delayed_jobs, :force => true do |t|
+      t.integer  :priority, :default => 0      # Allows some jobs to jump to the front of the queue
+      t.integer  :attempts, :default => 0      # Provides for retries, but still fail eventually.
+      t.text     :handler                      # YAML-encoded string of the object that will do work
+      t.text     :last_error                   # reason for last failure (See Note below)
+      t.string   :locked_by                    # Who is working on this object (if locked)
+      t.datetime :run_at                       # When to run. Could be Time.now for immediately, or sometime in the future.
+      t.datetime :locked_at                    # Set when a client is working on this object
+      t.datetime :failed_at                    # Set when all retries have failed (actually, by default, the record is deleted instead)
+      t.datetime :first_started_at             # Needed for task tracking
+      t.datetime :last_started_at              # Needed for task tracking
+      t.datetime :finished_at                  # Needed for when you are not deleting items from the table on completion
+      t.timestamps
     end
 
   end
--- init.rb
+++ init.rb
@@ -1,1 +1,2 @@
 require File.dirname(__FILE__) + '/lib/delayed_job'
+Delayed::Job.destroy_successful_jobs = false
